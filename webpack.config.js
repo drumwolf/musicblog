efe6c904ff43f166a1171866161a626b1dec3451
John Lee <drumwolf@gmail.com>
2021-01-02 11:05:28 -0800

    Enabled all routes to default back to index page

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,27 @@
-var path = require('path');
-var HtmlWebpackPlugin =  require('html-webpack-plugin');
+const path = require('path');
+const HtmlWebpackPlugin =  require('html-webpack-plugin');
 
 module.exports = {
-    entry : './src/index.js',
-    output : {
-        path : path.resolve(__dirname , 'dist'),
-        filename: 'index_bundle.js'
-    },
-    module : {
-        rules : [
-            {test : /\.(js)$/, use:'babel-loader'},
-            {test : /\.css$/, use:['style-loader', 'css-loader']},
-            {test : /\.(md)$/, use:'raw-loader'},
-        ]
-    },
-    mode:'development',
-    plugins : [
-        new HtmlWebpackPlugin ({
-            template : 'public/index.html'
-        })
+  entry : './src/index.js',
+  output : {
+    path : path.resolve(__dirname , 'dist'),
+    filename: 'index_bundle.js',
+    publicPath: '/'
+  },
+  module : {
+    rules : [
+      {test : /\.(js)$/, use:'babel-loader'},
+      {test : /\.css$/, use:['style-loader', 'css-loader']},
+      {test : /\.(md)$/, use:'raw-loader'},
     ]
-
+  },
+  devServer: {
+    historyApiFallback: true
+  },
+  mode:'development',
+  plugins : [
+    new HtmlWebpackPlugin ({
+        template : 'public/index.html'
+    })
+  ]
 }
